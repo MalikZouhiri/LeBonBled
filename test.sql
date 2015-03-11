f6534c3fc83f677c1bc9fe661d61d295fc43d659
Malik Zouhiri <zouhiri.malik@gmail.com>
2015-03-11 10:31:46 +0100

    update test.sql
    Dernière version de la bd

--- test.sql
+++ test.sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.1.12
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 04 Mars 2015 à 09:30
+-- Généré le :  Mer 11 Mars 2015 à 10:29
 -- Version du serveur :  5.6.16
 -- Version de PHP :  5.5.11
 
@@ -37,7 +37,16 @@ CREATE TABLE IF NOT EXISTS `annonce` (
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `proprietaire` (`proprietaire`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `annonce`
+--
+
+INSERT INTO `annonce` (`id`, `titre`, `prix`, `date_publication`, `description`, `proprietaire`, `photo`, `active`) VALUES
+(1, 'Test', 10, '2015-03-11 09:06:11', 'Test', 'admin', 'Image/delete.png', 1),
+(2, 'titre', 5, '2015-03-11 09:10:20', 'Description', 'Admin', 'photo', 1),
+(3, 'tit', 15, '2015-03-11 10:10:18', 'reerrezkrekl', 'toto', 'Image/3.jpeg', 0);
 
 -- --------------------------------------------------------
 
@@ -54,17 +63,14 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `admin` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Contraintes pour les tables exportées
+-- Contenu de la table `utilisateurs`
 --
 
---
--- Contraintes pour la table `annonce`
---
-ALTER TABLE `annonce`
-  ADD CONSTRAINT `annonce_ibfk_1` FOREIGN KEY (`proprietaire`) REFERENCES `utilisateurs` (`nom`);
+INSERT INTO `utilisateurs` (`id`, `nom`, `tel`, `password`, `mail`, `admin`) VALUES
+(4, 'admin', '0600000000', '21232f297a57a5a743894a0e4a801fc3', 'badara@admin.fr', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
