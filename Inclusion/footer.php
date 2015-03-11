7ef3b23bea611028dc3fb0271f1eedaf25be05fa
RAMONI Ted <eagleframe@gmail.com>
2015-03-11 11:30:42 +0100

    couleur des barres vert-menthe

--- Inclusion/footer.php
+++ Inclusion/footer.php
@@ -3,7 +3,7 @@ echo'
 
     <footer class="container-fluid navbar-fixed-bottom footbottom">
       <div class="container">
-        <center><p class="text-muted">LeBonBled © 2015 - Made by AZZA - COUDRAY - RAMONI - ZOUHIRI</p></center>
+        <center><p style="color:white" class="text-muted">LeBonBled © 2015 - Made by AZZA - COUDRAY - RAMONI - ZOUHIRI</p></center>
       </div>
     </footer>';
 ?>
--- Inclusion/gestion.php
+++ Inclusion/gestion.php
@@ -1,36 +1,48 @@
+<style type="text/css">
+
+.navbar-green {
+    background-color: #16B84E;
+    border-color: #16B84E;
+}
+
+.text-green {
+    color: #ffffff;
+}
+</style>
+
 <?php
 echo'
-<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar navbar-green navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span>
  <span class="icon-bar"></span>
  <span class="icon-bar"></span>
-            </button> <a class="navbar-brand" href="index.php">LeBonBled</a>
+            </button> <a class="navbar-brand text-green" href="index.php">LeBonBled</a>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class=""><a class="" href="index.php">Accueil</a>
+                <li class=""><a class="text-green" href="index.php">Accueil</a>
                 </li>
-                <li><a class="" href="recherche.php">Recherche</a>
+                <li><a class="text-green" href="recherche.php">Recherche</a>
                 </li>
-                <li><a class="" href="publier.php">Publier une annonce</a>
+                <li><a class="text-green" href="publier.php">Publier une annonce</a>
                 </li>';
 				if(isset($_SESSION['login']))
 				{
 					if($_SESSION['login']=='admin'){
-                        echo'<li><a class="" href="liste_inscrit.php">Panel Admin</a></li>
-                        <li><a class="" href="deconexion.php">Deconnexion</a></li>';
+                        echo'<li><a class="text-green" href="liste_inscrit.php">Panel Admin</a></li>
+                        <li><a class="text-green" href="deconexion.php">Deconnexion</a></li>';
                     }
                     else{
-                        echo'<li><a class="" href="compte.php">Mon Compte</a></li>
-                        <li><a class="" href="deconexion.php">Deconnexion</a></li>';
+                        echo'<li><a class="text-green" href="compte.php">Mon Compte</a></li>
+                        <li><a class="text-green" href="deconexion.php">Deconnexion</a></li>';
                     }
 				}
 				else
 				{
-				    echo'<li><a class="" href="inscription.php">Inscription</a></li>
-					<li><a class="" href="connexion.php">Connexion</a></li>';
+				    echo'<li><a class="text-green" href="inscription.php">Inscription</a></li>
+					<li><a class="text-green" href="connexion.php">Connexion</a></li>';
 				}
 			echo'
             </ul>
