2fed229c64494698b225908141d20228e3c90ca2
RAMONI Ted <eagleframe@gmail.com>
2015-03-18 10:47:58 +0100

    NOK - suppression de son compte

--- compte.php
+++ compte.php
@@ -28,14 +28,63 @@
             body {  padding-top: 50px;
 			}
         </style>
+        <script type="text/javascript">
+            function confirmation() {
+            if (confirm("Supprimer votre compte ?")) {
+            }
+        }  
+        </script>
     </head>
 
     
-    <body  >      
-<?php include("Inclusion/gestion.php");
+    <body>  
+<?php
+    include("Inclusion/gestion.php");
+    include("configuration.php");
+
+    if(isset($_GET['delete']) && isset($_GET['cp']))
+    {         
+        $nom= mysql_real_escape_string($_GET['cp']);
+        $nom=trim($nom);
+        $connexion2=connexion();
+        $sql2="DELETE FROM utilisateurs WHERE nom=".$nom;
+        $requete2=mysqli_query($connexion2,$sql2);
+        header('Location:index.php');
+        
+    }
+
+    if(isset($_GET['nom']) && isset($_GET['action']) && isset($_GET['id']) )
+    {
+        $nom= mysql_real_escape_string($_GET['nom']);
+        $nom=trim($nom);
+        if($_GET['action']=="pause"){
+            $connexion2=connexion();
+            $id= mysql_real_escape_string($_GET['id']);
+            $id=trim($id);
+            $sql2="UPDATE annonce SET active = '0' WHERE id=".$id;
+            $requete2=mysqli_query($connexion2,$sql2);
+            header("Refresh:0; url=compte.php");        }       
+        if($_GET['action']=="resume"){
+            $connexion2=connexion();
+            $id= mysql_real_escape_string($_GET['id']);
+            $id=trim($id);
+            $sql2="UPDATE annonce SET active = '1' WHERE id=".$id;
+            $requete2=mysqli_query($connexion2,$sql2);
+            header("Refresh:0; url=compte.php");        }       
+        if($_GET['action']=="supp"){
+            $connexion2=connexion();
+            $id= mysql_real_escape_string($_GET['id']);
+            $id=trim($id);
+            $sql2="DELETE FROM annonce WHERE id=".$id;
+            $requete2=mysqli_query($connexion2,$sql2);
+            header("Refresh:0; url=compte.php");        }       
+    }
+?>  
+
+<?php
 
 		$login=$_SESSION['login'];
-		$connexion=mysql_connect("localhost","root","");
+		$connexion=connexion();
 		$ok=mysql_select_db("test");
 		$requete="SELECT * FROM utilisateurs WHERE nom ='$login'";
 		$sql = mysql_query($requete) or die(mysql_error());
@@ -48,7 +97,44 @@
                 <div class="panel-heading">Utilisateur : <?php echo $result['nom'] ?></div>
                 <div class="panel-body"> Téléphone : <?php echo $result['tel'] ?> <br />
 				Adresse E-Mail : <?php echo $result['mail'] ?> <br />
+                Supprimer votre compte - <a onclick='confirmation()' href='compte.php?delete=ok&cp=<?php echo $result['nom'];?>'><img width="50" height="50" title='Supprimer votre compte' src='Image/compte.png'/></a>
+
 				</div>
+                <br/><br />
+                <?php 
+                    $nom= mysql_real_escape_string($result['nom']);
+                    $nom=trim($nom);
+                    ?>
+                     <h1 class="">Vos annonces</h1>
+                     <center>
+                     <?php
+                        $sql="SELECT * FROM annonce where proprietaire = '".$nom."'";
+                        $requete=mysqli_query($connexion,$sql);
+                        echo "<br /> <br />
+                        <table class='table table-striped'>
+                        <tr>
+                            <td style='background-color:gray;padding-right:100px;border-right:1px solid white;'>
+                                <p style='color:white'>Titre</p></td><td style='border-right:1px solid white;background-color:gray;padding-right:100px;'>
+                                <p style='color:white'>Date de Publication</p></td><td style='background-color:gray;padding-right:100px;border-right:1px solid white;'>
+                                <p style='color:white'>Description</p></td><td align='center' style='background-color:gray;'>
+                                <p style='color:red'>Actions</p></td>
+                            </td>
+                        </tr>";
+                        while($data=mysqli_fetch_array($requete))
+                        {
+                                 echo "<tr>
+                                 <td>".$data[1]."</td>
+                                 <td>".$data[3]."</td>
+                                 <td>".$data[4]."</td>
+                                 <td align='center'>";
+                                 if($data[7]==1){
+                                    echo "<a href='compte.php?nom=".$nom."&action=pause&id=".$data[0]."'><img title='Desactiver annonce' src='Image/pause.gif'/></a>                        
+                                 ";}
+                                 echo "<a href='compte.php?nom=".$nom."&action=supp&id=".$data[0]."'><img title='Supprimer annonce' src='Image/delete.png'/></a></tr>";
+                        }
+                    echo "</table>";
+                ?>                    
+                <br/> <br/>
             </div>
     </div>
 </div>
